52d0cc120c2dc02893350652d2303aeef45be881
dave070517 <stephenon56@gmail.com>
2024-08-13 03:03:15 +0100

    fav screen

--- lib/pages/favourite_screen.dart
+++ lib/pages/favourite_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class FavouriteScreen extends StatefulWidget {
   const FavouriteScreen({super.key});
@@ -10,8 +11,75 @@ class FavouriteScreen extends StatefulWidget {
 class _FavouriteScreenState extends State<FavouriteScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.purple,
+    return Scaffold(
+      // backgroundColor: Colors.purple,
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {
+              // Handle icon press
+            },
+            icon: const Icon(
+              Icons.shopping_cart,
+            ), // Replace with your desired icon
+          ),
+        ],
+        title: Text(
+          'Favourites',
+          style: GoogleFonts.nunito(
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 400,
+              width: 550,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Image.asset(''),
+                        const Column(
+                          children: [
+                            // TODO name of item collected from the api
+                            Text('Apple Smartwatch Series 6'),
+                            Padding(
+                              padding: EdgeInsets.symmetric(vertical: 6),
+                            ),
+                            Row(
+                              children: [Text('N 8000'), Text('N 000,000')],
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                    const Text('In stock'),
+                    Row(
+                      children: [
+                        Container(
+                          color: Colors.white10,
+                          child: const Text('Remove'),
+                        ),
+                        const Spacer(),
+                        Container(
+                          color: Colors.blue,
+                          width: 100,
+                          height: 20,
+                          child: const Text('Buy Now'),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
